125124383252de3c03be461fcf0a344269bb8443
Jami Couch <jami.couch@twinsunsolutions.com>
2023-03-03 15:54:37 -0600

    Fix package paths

--- lib/critic.dart
+++ lib/critic.dart
@@ -2,14 +2,14 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:inventiv_critic_flutter/api.dart';
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:inventiv_critic_flutter/modal/app.dart';
 import 'package:inventiv_critic_flutter/modal/bug_report.dart';
 import 'package:inventiv_critic_flutter/modal/device.dart';
 import 'package:inventiv_critic_flutter/modal/ping_request_modal.dart';
 import 'package:inventiv_critic_flutter/modal/ping_response.dart';
 import 'package:inventiv_critic_flutter/modal/report_request_modal.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class Critic {
   //singleton set-up

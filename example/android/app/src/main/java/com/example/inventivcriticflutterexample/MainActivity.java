082e638ba83d2ea44f7d4fef1f710c4b07c96a17
DESKTOP-7P3QRQP\gekko <gekko3558@gmail.com>
2023-12-05 13:59:06 -0600

    running in android studio

--- example/android/app/src/main/java/com/example/inventivcriticflutterexample/MainActivity.java
+++ example/android/app/src/main/java/com/example/inventivcriticflutterexample/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.inventivcriticflutterexample;
 
 import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
   }
 }
